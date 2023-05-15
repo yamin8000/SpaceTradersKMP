d1fc1d9068931e6165dfe313b7d676964cfb0875
YaMiN <yamin8000@yahoo.com>
2023-05-15 18:26:41 +0330

    added coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
             }
         }
         val commonTest by getting
