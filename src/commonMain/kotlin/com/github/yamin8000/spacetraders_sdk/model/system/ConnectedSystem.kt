e81a82f11c1f40dbe07725b905268bb4ccaf8cd9
YaMiN <yamin8000@yahoo.com>
2023-05-18 20:36:41 +0330

    closes #45

--- src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/system/ConnectedSystem.kt
+++ src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/system/ConnectedSystem.kt
@@ -3,12 +3,12 @@ package com.github.yamin8000.spacetraders_sdk.model.system
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class System(
+data class ConnectedSystem(
     override val symbol: String,
     override val sectorSymbol: String,
     override val type: SystemType,
+    val factionSymbol: String? = null,
     override val x: Int,
     override val y: Int,
-    val waypoints: List<SystemWaypoint>,
-    val factions: List<SystemFaction>
+    val distance: Int
 ) : CoreSystem()
