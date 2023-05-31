bd7219fef14f2d0a6aa1d6bf5b577a2f8ac3f7ab
YaMiN <yamin8000@yahoo.com>
2023-05-31 12:20:33 +0330

    updated some libs

--- src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/general/RegisterRequestBody.kt
+++ src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/general/RegisterRequestBody.kt
@@ -3,9 +3,9 @@ package com.github.yamin8000.spacetraders_sdk.model.general
 import com.github.yamin8000.spacetraders_sdk.model.faction.FactionSymbol
 
 /**
- *@param symbol How other agents will see your ships and information.
- *
- * Greater equal than 3 characters and Lesser equal than 14 characters
+ * @param faction The faction you choose determines your headquarters.
+ * @param symbol How other agents will see your ships and information.
+ * @param email Your email address. This is used if you reserved your call sign between resets.
  */
 data class RegisterRequestBody(
     val faction: FactionSymbol,
--- src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/web/api/GeneralAPIs.kt
+++ src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/web/api/GeneralAPIs.kt
@@ -12,10 +12,26 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 
 object GeneralAPIs {
+    /**
+     * Returns a simple status of the game server.
+     */
     suspend fun GameClient.simpleStatus() = client.get(BASE / "game/status").bodyAsText()
 
+    /**
+     * Returns the status of the game server.
+     */
     suspend fun GameClient.status(): Status = get("").body()
 
+    /**
+     * Creates a new agent and ties it to a temporary Account.
+     * The agent symbol is a 3-14 character string that will represent your agent.
+     * This symbol will prefix the symbol of every ship you own.
+     * Agent symbols will be cast to all uppercase characters.
+     *
+     * A new agent will be granted an authorization token,
+     * a contract with their starting faction,
+     * a command ship with a jump drive, and one hundred thousand credits.
+     */
     suspend fun GameClient.register(
         registerRequestBody: RegisterRequestBody
     ): RegisterResponse = client.post(BASE / "register") { setBody(registerRequestBody) }.data()
