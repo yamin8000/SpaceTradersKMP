461cc78f4079938775e6c8c53da2c09ca1ef7dce
YaMiN <yamin8000@yahoo.com>
2023-07-29 04:18:52 +0330

    closes #22

---
+++ src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/ship/ShipFuel.kt
@@ -0,0 +1,10 @@
+package com.github.yamin8000.spacetraders_sdk.model.ship
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ShipFuel(
+    val current: Int,
+    val capacity: Int,
+    val consumed: ShipFuelConsumption
+)
---
+++ src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/ship/ShipFuelConsumption.kt
@@ -0,0 +1,10 @@
+package com.github.yamin8000.spacetraders_sdk.model.ship
+
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ShipFuelConsumption(
+    val amount: Int,
+    val timestamp: LocalDateTime
+)
