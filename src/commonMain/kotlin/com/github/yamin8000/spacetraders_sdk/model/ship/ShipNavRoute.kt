ba1502ce4214211eb2b178861317ae1abe05c85b
YaMiN <yamin8000@yahoo.com>
2023-08-07 19:41:23 +0330

    closes #27

---
+++ src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/ship/ShipNavRoute.kt
@@ -0,0 +1,13 @@
+package com.github.yamin8000.spacetraders_sdk.model.ship
+
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ShipNavRoute(
+    val destination: ShipNavRouteWaypoint,
+    val departure: ShipNavRouteWaypoint,
+    val departureTime: LocalDateTime,
+    val arrival: LocalDateTime
+)
