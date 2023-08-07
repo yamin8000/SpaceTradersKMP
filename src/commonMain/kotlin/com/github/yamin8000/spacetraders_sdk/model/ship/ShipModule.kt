620bfc5d230037a289075cf3f0605c1a2ce4a7a4
YaMiN <yamin8000@yahoo.com>
2023-08-07 19:30:31 +0330

    doc updated

--- src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/ship/ShipModule.kt
+++ src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/ship/ShipModule.kt
@@ -2,6 +2,9 @@ package com.github.yamin8000.spacetraders_sdk.model.ship
 
 import kotlinx.serialization.Serializable
 
+/**
+ * A module can be installed in a ship and provides a set of capabilities such as storage space or quarters for crew. Module installations are permanent.
+ */
 @Serializable
 data class ShipModule(
     val symbol: ShipModuleSymbol,
