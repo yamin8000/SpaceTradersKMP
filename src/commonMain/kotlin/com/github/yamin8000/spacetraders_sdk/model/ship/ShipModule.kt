6814a1c1bb7761c383fa24b24e01dfef640b575a
YaMiN <yamin8000@yahoo.com>
2023-08-07 19:22:43 +0330

    closes #23

--- src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/ship/ShipModule.kt
+++ src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/ship/ShipModule.kt
@@ -3,6 +3,11 @@ package com.github.yamin8000.spacetraders_sdk.model.ship
 import kotlinx.serialization.Serializable
 
 @Serializable
-class ShipModule {
-
-}
+data class ShipModule(
+    val symbol: ShipModuleSymbol,
+    val capacity: Int,
+    val range: Int,
+    val name: String,
+    val description: String,
+    val requirements: ShipRequirements
+)
