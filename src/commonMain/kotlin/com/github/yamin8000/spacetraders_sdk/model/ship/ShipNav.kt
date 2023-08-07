c0dab1a0e394ee668ac19f6c86a5f00850492b06
YaMiN <yamin8000@yahoo.com>
2023-08-07 19:44:28 +0330

    closes #25

--- src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/ship/ShipNav.kt
+++ src/commonMain/kotlin/com/github/yamin8000/spacetraders_sdk/model/ship/ShipNav.kt
@@ -3,6 +3,10 @@ package com.github.yamin8000.spacetraders_sdk.model.ship
 import kotlinx.serialization.Serializable
 
 @Serializable
-class ShipNav {
-
-}
+data class ShipNav(
+    val systemSymbol: String,
+    val waypointSymbol: String,
+    val route: ShipNavRoute,
+    val status: ShipNavStatus,
+    val flightMode: ShipNavFlightMode
+)
