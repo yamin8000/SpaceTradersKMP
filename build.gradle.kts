8d9acb2846e3dd9e60cb50e76cd844926885f82f
YaMiN <yamin8000@yahoo.com>
2023-10-13 04:27:11 +0330

    updated libs and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("multiplatform") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("multiplatform") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.10"
     id("convention.publication")
 }
 
 group = "com.github.yamin8000"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
     mavenCentral()
@@ -36,11 +36,11 @@ kotlin {
 
 
     sourceSets {
-        val ktorVersion = "2.3.3"
+        val ktorVersion = "2.3.5"
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
                 implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
