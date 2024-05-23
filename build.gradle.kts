5d9e87b79a4b9ed5b0a2c50e32454a3b9305c3fd
Yamin Siahmargooei <yamin8000@yahoo.com>
2024-05-24 01:26:47 +0330

    updated some libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,10 +37,10 @@ kotlin {
     }
 
     sourceSets {
-        val ktorVersion = "2.3.8"
+        val ktorVersion = "2.3.10"
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
                 implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
